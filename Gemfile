c19eaa82b3a7f51cc71c163246ff34af7f59ea1b
Nestor Castillo <nes_23@yahoo.com>
2016-06-25 18:12:07 -0600

    configure middleman with bourbon, neat, bitters

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
+gem 'middleman-sprockets'
+gem 'middleman-autoprefixer'
+gem 'bitters'
+gem 'bourbon'
+gem 'neat'
--- config.rb
+++ config.rb
@@ -1,22 +1,17 @@
-###
-# Page options, layouts, aliases and proxies
-###
+# General configuration
+activate :sprockets
+activate :directory_indexes
+activate :autoprefixer
 
-# Per-page layout changes:
-#
-# With no layout
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+set :layout, "layouts/application"
+
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
 
 # Reload the browser automatically whenever files change
 configure :development do
@@ -36,9 +31,5 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :relative_assets
 end
